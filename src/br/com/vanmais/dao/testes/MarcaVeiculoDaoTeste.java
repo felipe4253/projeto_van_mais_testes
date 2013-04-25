59da36e23a53840e4cf225407b41f04db9c82708
felipe4253 <felipe4253@gmail.com>
2013-04-25 12:07:33 -0300

    teste

--- src/br/com/vanmais/dao/testes/MarcaVeiculoDaoTeste.java
+++ src/br/com/vanmais/dao/testes/MarcaVeiculoDaoTeste.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import br.com.vanmais.bancodedados.dao.MarcaVeiculoDAO;
 import br.com.vanmais.bancodedados.entidade.MarcaVeiculoEntidade;
 import br.com.vanmais.bancodedados.util.HibernateUtil;
-
 /**
  * @author fassilva
  *
@@ -23,16 +22,22 @@ public class MarcaVeiculoDaoTeste{
 	@Test
 	public void teste(){
 		sessao = HibernateUtil.getSession(sessao);
+		sessao.beginTransaction();
 		MarcaVeiculoDAO mvDao = new MarcaVeiculoDAO(sessao);
 		MarcaVeiculoEntidade mv = new MarcaVeiculoEntidade();
-		mv.setId(0L);
-		mv.setDescMarca("Fiat");
-		mvDao.salvar(mv);
+		mv.setId(null);
+		mv.setDescMarca("Jac Motors");
+		Long codigo = mvDao.salvar(mv);
+		System.out.println("\n\nTESTE\n\n\tCodigo = "+codigo+"\n\nFIM TESTE");
+		MarcaVeiculoEntidade mve = mvDao.buscarPeloId(codigo);
+		sessao.getTransaction().commit();
+		sessao.close();
+		System.out.println("\n\nTESTE\n\n\tMarca = "+mve.getDescMarca()+
+				"n\tCodigo Marca = "+mve.getId()+"\n\nFIM TESTE");
 		
-		MarcaVeiculoEntidade mve = mvDao.buscarPeloId(0L);
+		
 		Assert.assertNotNull(mve);
+		
 	}
-	
-	
 	
 }
